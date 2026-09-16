fbddfbfff7e55f468ea29b7e6bab54ccd5617fea
Raul-Araujo <raul31_lpcn@hotmail.com>
2026-09-15 23:30:52 -0300

    add solution fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,12 +5,18 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  if (days >= 7) {
-    return days * 40 - 50;
-  } else if (days >= 3) {
-    return days * 40 - 20;
+  const LONG_TIME = 7;
+  const MID_TIME = 3;
+  const LONG_TIME_DISCOUNT = 50;
+  const MID_TIME_DISCOUNT = 20;
+  const BASE_VALUE = 40;
+
+  if (days >= LONG_TIME) {
+    return days * BASE_VALUE - LONG_TIME_DISCOUNT;
+  } else if (days >= MID_TIME) {
+    return days * BASE_VALUE - MID_TIME_DISCOUNT;
   } else {
-    return days * 40;
+    return days * BASE_VALUE;
   }
 }
 
